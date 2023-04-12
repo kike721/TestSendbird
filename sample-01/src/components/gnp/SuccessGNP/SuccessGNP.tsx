0eb940c226012c063d9c993aa63aa564bc0c817e
Valeria Pérez Jaimes <danielacarbajalnava@INFRAESTRUCTURAs-MacBook-Pro.local>
2023-04-12 08:53:01 -0500

    Add styles component successGNP

--- sample-01/src/components/gnp/SuccessGNP/SuccessGNP.tsx
+++ sample-01/src/components/gnp/SuccessGNP/SuccessGNP.tsx
@@ -30,8 +30,56 @@ const Text = styled.p`
 
 const ButtonBack = styled.button`
   background: #FFE9DD;
-  padding: 1em;
-  corder-radius: 20px;
+  border-radius: 24px;
+  color: #FF6E1F;
+  border: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 700;
+  justify-content: center;
+  margin-bottom: 1em;
+  max-width: 250px;
+  padding: 1.5em;
+  text-align: center;
+  width: 100%;
+  cursor: pointer;
+  transition: all 0.5s;
+
+  & img {
+    width: 25px;
+    margin: 0 1em;
+  }
+
+  &:hover {
+    filter: brightness(95%);
+  }
+`;
+
+const ButtonSession = styled.button`
+  align-items: center;
+  background: #fff;
+  border-radius: 24px;
+  border-radius: 24px;
+  border: solid 2px #FF6E1F;
+  color: #FF6E1F;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+  font-weight: 700;
+  justify-content: center;
+  margin-bottom: 1em;
+  max-width: 250px;
+  padding: 1.5em;
+  text-align: center;
+  transition: all 0.5s;
+  width: 100%;
+
+  &:hover {
+    filter: brightness(95%);
+  }
 `;
 
 const SuccessGNP = ({
@@ -47,11 +95,15 @@ const SuccessGNP = ({
         <ImgLogo src={ImgSuccess} alt="success exitoso imagen" />
         <Text>Póliza impresa</Text>
       </ContentLogo>
-      <ButtonBack onClick={onClickBackMenu}>
-        <img src={Shape} alt="back"></img>
-        Regresar al menú
-      </ButtonBack>
-      <button onClick={onClickEndSession}>Finalizar sesión</button>
+      <div>
+        <ButtonBack onClick={onClickBackMenu}>
+          <img src={Shape} alt="back"></img>
+          Regresar al menú
+        </ButtonBack>
+        <ButtonSession onClick={onClickEndSession}>
+          Finalizar sesión
+        </ButtonSession>
+      </div>
       <HeaderGNP />
     </Content>
   )
