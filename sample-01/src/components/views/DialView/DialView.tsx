535181c5bde7b303c4233ac197a041292b5148d6
José Enrique Durán García <kukicobain@Mac-mini-de-Jose.local>
2023-05-16 07:37:01 -0600

    Fix number whatsapp

--- sample-01/src/components/views/DialView/DialView.tsx
+++ sample-01/src/components/views/DialView/DialView.tsx
@@ -130,7 +130,7 @@ const DialView: React.FC<DialViewProps> = props => {
   }
 
   const handleOnClickWhatsappButton = () => {
-    window.open('https://wa.me/13325065874?text=Prueba%20motorola');
+    window.open('https://wa.me/523325065874?text=Prueba%20motorola');
   }
 
   return (
